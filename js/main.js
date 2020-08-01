23ff8bd324077496db007efcb6d8cbd346a3d66c
andreySavchenko <58919772+sava911@users.noreply.github.com>
2020-08-02 01:37:20 +0300

    slider slick

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-​$(function(){
+$(function () {
 
-    $('.fairy-tail__slider').slick({});
+    $('.fairy-tail__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/arrow-prev.svg" alt=""> </button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow-next.svg" alt=""> </button>',
+        autoplay: true,
+        fade: true
+    });
 
 });
-
