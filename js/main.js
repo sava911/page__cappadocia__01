a725c796f410c1f757976b2aff02dc360ab44fc6
andreySavchenko <58919772+sava911@users.noreply.github.com>
2020-08-03 16:46:54 +0300

    day 3 hotel new

--- js/main.js
+++ js/main.js
@@ -7,4 +7,11 @@ $(function () {
         fade: true
     });
 
+    $('.our-trip__sider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/arrow-prev.svg" alt=""> </button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow-next.svg" alt=""> </button>',
+        autoplay: true,
+        fade: true
+    })
+
 });
